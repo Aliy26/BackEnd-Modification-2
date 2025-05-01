c95abdafa46d64e30c28fd4fa78ef6db21db2abc
Umar_Aliy <umaraliy092@gmail.com>
2025-05-01 21:24:54 +0900

    fix: modified few errors

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -14,8 +14,10 @@ async function bootstrap() {
   app.enableCors({ origin: true, credentials: true });
 
   app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
-  // app.use("/uploads", express.static("./uploads"));
   app.use("/uploads", express.static(join(__dirname, "..", "uploads")));
+  // console.log("Serving static files from:", join(__dirname, "..", "uploads"));
+
+  app.use("/uploads", express.static("./uploads"));
 
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT_API ?? 3000);
