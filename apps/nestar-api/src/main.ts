3bbea16269e2688d9c0eec323505fd2f1c8166bd
Umar_Aliy <umaraliy092@gmail.com>
2025-05-01 13:39:51 +0900

    fix: modified main.ts file

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -5,6 +5,7 @@ import { LoggingInterceptor } from "./libs/interceptor/Logging.interceptor";
 import { graphqlUploadExpress } from "graphql-upload";
 import * as express from "express";
 import { WsAdapter } from "@nestjs/platform-ws";
+import { join } from "path";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,8 @@ async function bootstrap() {
   app.enableCors({ origin: true, credentials: true });
 
   app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
-  app.use("/uploads", express.static("./uploads"));
+  // app.use("/uploads", express.static("./uploads"));
+  app.use("/uploads", express.static(join(__dirname, "..", "uploads")));
 
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT_API ?? 3000);
